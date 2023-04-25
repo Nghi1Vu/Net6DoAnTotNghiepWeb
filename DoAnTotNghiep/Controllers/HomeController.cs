a047056fffa57023543b16489355a867a808edee
Minhthai.2507 <99379249+Sinner-King@users.noreply.github.com>
2023-04-25 19:21:56 +0700

    Phiếu đánh giá

--- DoAnTotNghiep/Controllers/HomeController.cs
+++ DoAnTotNghiep/Controllers/HomeController.cs
@@ -162,6 +162,12 @@ namespace DoAnTotNghiep.Controllers
         {
             return View();
         }
+
+        public IActionResult BieuMauDanhGiaRenLuyen()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
