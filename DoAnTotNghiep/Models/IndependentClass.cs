b585e336f2209bc479b176fbd3fdd1c130b61b85
Vu Thanh Nghi <nghivu201@gmail.com>
2023-05-27 21:36:21 +0700

    edit and dp getic

--- DoAnTotNghiep/Models/IndependentClass.cs
+++ DoAnTotNghiep/Models/IndependentClass.cs
@@ -2,15 +2,16 @@
 {
     public class IndependentClass
     {
-        public int COSTS { get; set; }
+        public int Amount { get; set; }
+        public string SSSV { get; set; }
+        public int DayStudy { get; set; }
         public string RoomName { get; set; }
-        public string Description { get; set; }
+        public string timeday { get; set; }
         public string ClassName { get; set; }
         public string ClassCode { get; set; }
         public string Teachername { get; set; }
         public DateTime StartDate { get; set; }
-        public string Credits { get; set; }
-        public string MaxStudent { get; set; }
-   
+        public decimal Credits { get; set; }
+
     }
 }
